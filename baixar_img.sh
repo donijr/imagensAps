2ba31b410233d41231982b6d986abfc02f617d4f
donijr <donizetigmjr@gmail.com>
2020-10-28 22:50:09 -0300

    tempo para baixar imagens = 1s

--- baixar_img.sh
+++ baixar_img.sh
@@ -4,15 +4,18 @@ do
     #echo "00$i"
     var=00$i
     wget "https://github.com/donijr/imagensAps/blob/main/Imagens/$var.bmp"
+    sleep 1
  
   elif [ $i -lt 100 ];   then 
     #echo "0$i"
     var=0$i
     wget "https://github.com/donijr/imagensAps/blob/main/Imagens/$var.bmp"
+    sleep 1
  
   elif [ $i -lt 301 ];   then 
     #echo $i
     var=$i
     wget "https://github.com/donijr/imagensAps/blob/main/Imagens/$var.bmp"
+    sleep 1
   fi	
 done
